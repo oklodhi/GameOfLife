a07f2f346225a43c9597fdc4e69c7d37ff9b4849
Omer Khan <okhanlodhi@gmail.com>
2019-02-12 19:00:56 -0600

    Fixed middle index and corner index neighbor check

--- main.cpp
+++ main.cpp
@@ -13,10 +13,12 @@ int main() {
 
 	print_board(boardVect); 
 
-	srand(time(NULL));
+	srand((unsigned int)time(NULL));
 	random_state(boardVect); 
 
-	print_board(boardVect);  
+	print_board(boardVect); 
+
+	check_neighbors(boardVect);
 
 	return 0; 
 }
@@ -76,13 +78,13 @@ void print_board(std::vector<std::vector<int> > &vec) {
 		std::cout << "[ ";
 		for (std::size_t j = 0; j < vec[i].size(); j++) {
 			if (vec[i][j] == 0) {
-				std::cout << "  "; 
+				std::cout << "."; 
 			}
 			else if (vec[i][j] == 1) {
-				std::cout << "# ";
+				std::cout << "#";
 			}
 		}
-		std::cout << "]" << std::endl;
+		std::cout << " ]" << std::endl;
 	}
 }
 
@@ -92,34 +94,84 @@ void check_neighbors(std::vector<std::vector<int> > &vec) {
 		for (std::size_t j = 0; j < vec[i].size(); j++) {
 
 			// If current index is not a border index
-			if ((i > 0 && i < vec.size()) && (j > 0 && j < vec[i].size())) {
+			if ((i > 0 && j > 0) && (i < (vec.size() - 1) && j < (vec[i].size() - 1))) {
 				// loop through each neighboring index
-				for (int a = (i - 1); a <= (i + 1); a++) {
-					for (int b = (j - 1); b <= (j + 1); b++) {
+				//std::cout << "MIDDLE Row " << i << " Col " << j << std::endl;
+				for (std::size_t row = (i - 1); row < (i + 1); row++) {
+					for (std::size_t col = (j - 1); col < (j + 1); col++) {
 						// if not (i,j) aka center index
-						if ((a != i) && (b != j)) {
-							if (vec[a][b] == 1) {
-								// add to the count of alive neightbors,				will use this to perform conditionals
-								aliveNeighborCount++; 
-							}
+						if (((row != i) && (col != j)) && (vec[row][col] == 1)) {
+							//if (vec[row][col] == 1) {
+								// add to the count of alive neightbors, will use this to perform conditionals
+							aliveNeighborCount++; 
+							//}
 						}
 					}
 				}
+
 				// Checks neighbor index 1 by 1
 				/*if (vec[i + 1][j + 1] == 1) {
 					aliveNeighborCount++;
 				}*/
 			}
-			// else if border element, then must be corner index
-			else if ((i == 0 && j == 0) || (i == 0 && j == n) || (i == n && j == 0) || (i == n && j == n)) {
-
-			}
-			// or side index
-			else {
+			else { // If border index, then its either corner = 3 neighbors
+				if ((i == 0 && j == 0)) {
+					//std::cout << "CORNER TOP LEFT " << i << " Col " << j << std::endl;
+					if (vec[i][j + 1] == 1) {
+						aliveNeighborCount++; 
+					}
+					if (vec[i + 1][j] == 1) {
+						aliveNeighborCount++; 
+					}
+					if (vec[i + 1][j + 1] == 1) {
+						aliveNeighborCount++;
+					}
+				}
+				else if (i == 0 && j == (vec[i].size() - 1)) {
+					//std::cout << "CORNER TOP RIGHT " << i << " Col " << j << std::endl;
+					if (vec[i][j - 1] == 1) {
+						aliveNeighborCount++;
+					}
+					if (vec[i + 1][j - 1] == 1) {
+						aliveNeighborCount++;
+					}
+					if (vec[i + 1][j] == 1) {
+						aliveNeighborCount++;
+					}
+				}
+				else if (i == (vec.size() - 1) && j == 0) {
+					//std::cout << "CORNER BOT LEFT " << i << " Col " << j << std::endl;
+					if (vec[i - 1][j] == 1) {
+						aliveNeighborCount++;
+					}
+					if (vec[i - 1][j + 1] == 1) {
+						aliveNeighborCount++;
+					}
+					if (vec[i][j + 1] == 1) {
+						aliveNeighborCount++;
+					}
+				}
+				else if (i == (vec.size() - 1) && j == (vec[i].size() - 1)) {
+					//std::cout << "CORNER BOT RIGHT " << i << " Col " << j << std::endl;
+					if (vec[i - 1][j - 1] == 1) {
+						aliveNeighborCount++;
+					}
+					if (vec[i - 1][j] == 1) {
+						aliveNeighborCount++;
+					}
+					if (vec[i][j - 1] == 1) {
+						aliveNeighborCount++;
+					}
+				}
+				else { // or middle = 5 neighbors
+					//std::cout << "EDGE Row " << i << " Col " << j << std::endl;
+				}
 
 			}
 		}
 	}
+
+	std::cout << "Total alive neighbors are... " << aliveNeighborCount << std::endl; 
 }
 
 // generates next board state based on conditionals with AliveNeighborCount
